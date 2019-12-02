15da4b60fa2038d75e24aa539a3fe7d52ab34f78
Mandoortiz <43460190+Mandoortiz@users.noreply.github.com>
2019-12-02 00:26:33 -0700

    Add files via upload

--- login/report.php
+++ login/report.php
@@ -1,34 +1,10 @@
 <?php
-session_start();
-$host ="ilinkserver.cs.utep.edu";
-$db = 'f19_team9';
-$DBusername = 'raguilarsa';
-$DBpassword = '*utep2020!';
+        $mysqli = new mysqli('localhost', 'user', 'pass', 'mis_db');
 
-$conn = new mysqli($host,$DBusername,$DBpassword,$db);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (mysqli_connect_error()) {
+    echo mysqli_connect_error();
+    exit();
 }
 
-$username = $_SESSION["user"];
-$ID = $_SESSION["ID"];
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<link rel="stylesheet" type="text/css"
-	href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"></head>
-	<title>Generate Report</title>
-</head>
-<body>
-	Generate report!
-<nav>
-  <ul>
-    <!-- <li><a href="appointment.php">Create Appointment</a></li>
-    <li><a href="application.php">Create Application</a></li>-->
-    <li><a href="adminhome.php">Go back to home page</a></li>
-  </ul>
-</nav>
-</body>
-</html>
+$stck = $_POST['Stock'];
+$itm = $_POST['Items'];
