507f0ab89e14b2097ab46c9d7367591f4e334d26
d0xa <38441957+d0xa@users.noreply.github.com>
2019-12-02 18:46:57 -0700

    Add files via upload
    added new form tab  in the student home page.

---
+++ login/forms.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+
+<a href="forms/7--Change-of-Major--2018-Spring.pdf" download>Change Major Form</a> <br>
+<a href="forms/8--Course-Subsitution--.pdf" download>Course Subsitution Form</a> <br>
+<a href="forms/12--Petition-for-Minor.pdf" download>Petition for Minor Form</a> <br>
+<a href="forms/AcademicAdvisingForm.pdf" download>Academic Advising Form</a> <br>
+<a href="forms/AcademicAdvisingFormThesisDefence.pdf" download>Academic Advising Thesis Defense Form</a> <br>
+<a href="forms/Completion_Defense_Doctoral_Form.pdf" download>Completion Defense Doctoral Form</a> <br>
+<a href="forms/Faculty_Drop_Reinstatement.pdf" download>Faculty Drop Reinstatement</a> <br>
+<a href="forms/fast-track.pdf" download>Fast Track Form</a> <br>
+<a href="forms/GPA_Recalculation1.pdf" download>GPA Recalculation Form</a> <br>
+<a href="forms/Petition_for_Reinstatement_College_of_Engineering.pdf" download>Engineer Reinstatement Form</a> <br>
+<a href="forms/Thesis_Proposal_Form.pdf" download>Thesis Proposal Form</a> <br>
+<a href="forms/UG_Appl.pdf" download>Undergraduate Application</a> <br>
+<a href="forms/Verification_of_Eligibility.docx" download>Eligibility Verification Form</a> <br>
+<br>
+<nav>
+  <ul>
+    <li><a href="home.php">Go Back to home page</a></li>
+  </ul>
+</nav>
+</body>
+</html>
